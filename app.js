2d3e46a3b94a7754a64beabe861ff41bb3f5c7fe
nategrieb <nate@nategrieb.com>
2022-10-05 13:47:43 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.send(
     "<head><style>body { font-family: \"helvetica\"; }</style></head>\
-    <h1>This is Nate's app now MuhHAHAHAHA</h1>
-    <p>This app is being served from 349587349587 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>\
+    <h1>This is Nate's app now MuhHAHAHAHA</h1><p>This app is being served from 349587349587 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>\
     <p>Discover the location you are hitting with the dig command.</p>\
     <p>dig floral-smoke-4415.section.app</br>(look in the ANSWER section for a 3 letter city code)</p>\
     <p>Distribute your own Node Express app by visiting <a href=\"http://beta.section.io/docs/tutorials/frameworks/nodeexpress/\">http://beta.section.io/docs/tutorials/frameworks/nodeexpress/</a></p>",
